100a6de536f4d6bd84b0e2aa1035fb3fd2d04987
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-18 01:10:33 +0300

    Implement SlotMachineSpinItem

---
+++ Assets/Scripts/SlotMachine/View/SlotMachineSpinItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace ICouldGames.SlotMachine.View
+{
+    public class SlotMachineSpinItem : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer blurredItem;
+        [SerializeField] private SpriteRenderer cleanItem;
+
+        private void Awake()
+        {
+            ActivateCleanMode();
+        }
+
+        public void Init(Sprite blurredImage, Sprite cleanImage)
+        {
+            blurredItem.sprite = blurredImage;
+            cleanItem.sprite = cleanImage;
+        }
+
+        public void ActivateBlurredMode()
+        {
+            blurredItem.gameObject.SetActive(true);
+            cleanItem.gameObject.SetActive(false);
+        }
+
+        public void ActivateCleanMode()
+        {
+            cleanItem.gameObject.SetActive(true);
+            blurredItem.gameObject.SetActive(false);
+        }
+    }
+}
