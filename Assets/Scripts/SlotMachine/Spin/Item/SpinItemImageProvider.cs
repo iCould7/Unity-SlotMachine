902a23a8813f6aa250b5f073400e173abe84496e
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-18 00:20:47 +0300

    Implement SpinItemImageProvider

---
+++ Assets/Scripts/SlotMachine/Spin/Item/SpinItemImageProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ICouldGames.SlotMachine.Spin.Item
+{
+    public class SpinItemImageProvider : MonoBehaviour
+    {
+        public static SpinItemImageProvider Instance { get; private set; }
+
+        public bool IsReady { get; private set; } = false;
+
+        private Dictionary<SpinItemType, Sprite> _blurredSpinImages = new();
+        private Dictionary<SpinItemType, Sprite> _cleanSpinImages = new();
+
+        private const string SPIN_ITEM_IMAGES_FOLDER = "SpinItemImages/";
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+
+            Init();
+            IsReady = true;
+        }
+
+        private void Init()
+        {
+            foreach (var spinItemType in Enum.GetValues(typeof(SpinItemType)))
+            {
+                var itemId = (int) spinItemType;
+                var itemType = (SpinItemType) itemId;
+
+                _blurredSpinImages[itemType] = Resources.Load<Sprite>(SPIN_ITEM_IMAGES_FOLDER + $"SpinItem_Blurred_{itemId.ToString()}");
+                _cleanSpinImages[itemType] = Resources.Load<Sprite>(SPIN_ITEM_IMAGES_FOLDER + $"SpinItem_Clean_{itemId.ToString()}");
+            }
+        }
+
+        public Sprite GetCleanImage(SpinItemType itemType)
+        {
+            return _cleanSpinImages[itemType];
+        }
+
+        public Sprite GetBlurredImage(SpinItemType itemType)
+        {
+            return _blurredSpinImages[itemType];
+        }
+    }
+}
--- Assets/Scripts/SlotMachine/Spin/Item/SpinItemType.cs
+++ Assets/Scripts/SlotMachine/Spin/Item/SpinItemType.cs
@@ -1,11 +1,12 @@
 ﻿namespace ICouldGames.SlotMachine.Spin.Item
 {
+    // Don't change integer correspondents!
     public enum SpinItemType
     {
-        Ace,
-        Seven,
-        Jackpot,
-        Wild,
-        Bonus
+        Ace = 1,
+        Seven = 2,
+        Jackpot = 3,
+        Wild = 4,
+        Bonus = 5
     }
 }
