26d40b28a33aa1c0672de07c2c1720d31775f027
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-19 18:41:50 +0300

    Add random spin item enum generator

--- Assets/Scripts/SlotMachine/Spin/Item/SpinItemType.cs
+++ Assets/Scripts/SlotMachine/Spin/Item/SpinItemType.cs
@@ -1,4 +1,7 @@
-﻿namespace ICouldGames.SlotMachine.Spin.Item
+﻿using System;
+using Random = UnityEngine.Random;
+
+namespace ICouldGames.SlotMachine.Spin.Item
 {
     // Don't change integer correspondents!
     public enum SpinItemType
@@ -8,5 +11,14 @@
         Jackpot = 3,
         Wild = 4,
         Bonus = 5
+    }
+
+    public static class SpinItemTypeGenerator {
+        public static SpinItemType GetRandom()
+        {
+            var enumList = Enum.GetValues(typeof(SpinItemType));
+
+            return (SpinItemType) enumList.GetValue(Random.Range(0,enumList.Length));
+        }
     }
 }
