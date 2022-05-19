aae4d28ebbccf6467443c93865240fb60c7d8cc4
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-19 18:41:09 +0300

    Add LisExtentions for Shuffling

---
+++ Assets/Scripts/Extensions/System/Collections/Generic/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ICouldGames.Extensions.System.Collections.Generic
+{
+    public static class ListExtensions
+    {
+        private static readonly Random Random = new();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            for(var i=list.Count; i > 0; i--)
+            {
+                list.Swap(0, Random.Next(0, i));
+            }
+        }
+
+        public static void Swap<T>(this IList<T> list, int i, int j)
+        {
+            var temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+    }
+}
