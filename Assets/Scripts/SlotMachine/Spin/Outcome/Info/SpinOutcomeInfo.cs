febf848637cc084a564ba8a8658bf3f442185538
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-19 18:43:46 +0300

    Add SpinItemTypes list for each spinOutcomeInfo

--- Assets/Scripts/SlotMachine/Spin/Outcome/Info/SpinOutcomeInfo.cs
+++ Assets/Scripts/SlotMachine/Spin/Outcome/Info/SpinOutcomeInfo.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using ICouldGames.SlotMachine.Spin.Item;
 using UnityEngine;
 
 namespace ICouldGames.SlotMachine.Spin.Outcome.Info
 {
     [Serializable]
-    public class SpinOutcomeInfo
+    public class SpinOutcomeInfo : ISerializationCallbackReceiver
     {
         [SerializeField] private int id;
         [SerializeField] private SpinItemType firstItemType;
@@ -13,8 +14,22 @@ namespace ICouldGames.SlotMachine.Spin.Outcome.Info
         [SerializeField] private SpinItemType thirdItemType;
         [SerializeField] private int probability;
 
+        [NonSerialized] public List<SpinItemType> SpinItemTypes = new(3);
+
         public int Id => id;
         public int Probability => probability;
+
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+            SpinItemTypes.Clear();
+            SpinItemTypes.Add(firstItemType);
+            SpinItemTypes.Add(secondItemType);
+            SpinItemTypes.Add(thirdItemType);
+        }
 
         public override string ToString()
         {
