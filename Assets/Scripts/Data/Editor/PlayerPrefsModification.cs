1e7b277bae3f4368ef661d7d719e0fa59e2bd43d
Aykut Aslan <aslanaykut4@gmail.com>
2022-05-22 00:58:02 +0300

    Add PleayerPrefsClear action into editor toolbar

---
+++ Assets/Scripts/Data/Editor/PlayerPrefsModification.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ICouldGames.Data.Editor
+{
+    public static class PlayerPrefsModification
+    {
+        [MenuItem("Tools/Clear PlayerPrefs")]
+        public static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            EditorUtility.DisplayDialog("Info", "PlayerPrefs are successfully cleared.", "OK");
+        }
+    }
+}
